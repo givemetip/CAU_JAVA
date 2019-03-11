86a0a0776139800cb6b8376b557e33ac60b1b40c
CAU <CAU@10.210.129.104>
2019-03-11 15:31:15 +0900

    increment count of function to resolve 0 time

--- ex1/src/pack1/main.java
+++ ex1/src/pack1/main.java
@@ -3,6 +3,19 @@ package pack1;
 public class main {
 	public static void main(String[] args)
 	{
-		System.out.print("Hello World");
+		int a = 3;
+		long startTime = System.nanoTime();
+		for(int i=0;i<100;i++)
+		{
+			int result = a * a;
+		}
+		long endTime = System.nanoTime();
+		System.out.println("a * a time : " + (endTime-startTime));
+		
+		startTime = System.nanoTime();
+		for(int i=0;i<100;i++)
+			Math.pow(a, 2);
+		endTime = System.nanoTime();
+		System.out.println("pow a time : " + (endTime-startTime));
 	}
 }
